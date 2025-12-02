13ef9ce2fa88f6fd8849a1ff9f11151ebbf4029c
suneither <dangis.bakutis@gmail.com>
2025-12-02 11:56:22 +0200

    Solved Secret Entrance problem

--- Day_1_Secret-Entrance/Solution.cpp
+++ Day_1_Secret-Entrance/Solution.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int current_number = 50;
+int password = 0;
+
+char ExtractDirection(const string& rotation);
+int ExtractNumber(const string& rotation);
+void RotateLeft(const int& number);
+void RotateRight(const int& number);
+
 int main(){
-    cout << "Hello World";
+
+    vector<string> rotations;
+    rotations.emplace_back("L68");
+    rotations.emplace_back("L30");
+    rotations.emplace_back("R48");
+    rotations.emplace_back("L5");
+    rotations.emplace_back("R60");
+    rotations.emplace_back("L55");
+    rotations.emplace_back("L1");
+    rotations.emplace_back("L99");
+    rotations.emplace_back("R14");
+    rotations.emplace_back("L82");
+
+    for (const string& rotation : rotations)
+    {
+        if (ExtractDirection(rotation) == 'L')
+        {
+            RotateLeft(ExtractNumber(rotation));
+            if (current_number < 0) current_number += 100;
+        }else
+        {
+            RotateRight(ExtractNumber(rotation));
+            if (current_number >= 100) current_number -= 100;
+        }
+
+        if (current_number == 0)
+            password++;
+    }
+
+    cout << password << endl;
+
+}
+
+char ExtractDirection(const string& rotation)
+{
+    return rotation.at(0);
+}
+
+int ExtractNumber(const string& rotation)
+{
+    return stoi( rotation.substr(1, rotation.size() - 1));
+}
+
+void RotateLeft(const int& number)
+{
+    current_number -= number;
+}
+
+void RotateRight(const int& number)
+{
+    current_number += number;
 }
