2bc0737cc4ca15e80b11099281e6c9ecbf95037a
suneither <dangis.bakutis@gmail.com>
2025-12-03 13:41:14 +0200

    solved puzzle

--- Day_1_Secret-Entrance/Solution.cpp
+++ Day_1_Secret-Entrance/Solution.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -13,36 +14,29 @@ void RotateRight(const int& number);
 
 int main(){
 
-    vector<string> rotations;
-    rotations.emplace_back("L68");
-    rotations.emplace_back("L30");
-    rotations.emplace_back("R48");
-    rotations.emplace_back("L5");
-    rotations.emplace_back("R60");
-    rotations.emplace_back("L55");
-    rotations.emplace_back("L1");
-    rotations.emplace_back("L99");
-    rotations.emplace_back("R14");
-    rotations.emplace_back("L82");
+    fstream file("Input.txt");
 
-    for (const string& rotation : rotations)
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file.\n";
+        return 1;
+    }
+
+    string rotation;
+    while (std::getline(file, rotation))
     {
         if (ExtractDirection(rotation) == 'L')
-        {
             RotateLeft(ExtractNumber(rotation));
-            if (current_number < 0) current_number += 100;
-        }else
-        {
+        else
             RotateRight(ExtractNumber(rotation));
-            if (current_number >= 100) current_number -= 100;
-        }
+
+        if (current_number >= 100 || current_number < 0)
+            current_number %= 100;
 
         if (current_number == 0)
             password++;
     }
 
     cout << password << endl;
-
 }
 
 char ExtractDirection(const string& rotation)
